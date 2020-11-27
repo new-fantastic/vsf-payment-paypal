d6ffd8a460fee6786f604abdb8b18d85966d9c05
Łukasz <lukasz@newfantastic.com>
2020-11-27 14:37:41 +0100

    feat: back to previous shipping values

--- components/Button.vue
+++ components/Button.vue
@@ -151,7 +151,8 @@ export default {
       const freeShippingMinValue = config.shipping.freeShipping.minAmount
       if (this.isAp) shipping = 0
       else if (grandTotalsValue >= freeShippingMinValue) shipping = 0
-      else shipping = config.shipping.default
+      else if (storeCode === 'gb') shipping = config.shipping.default[storeCode]
+      else shipping = config.shipping.default.other
 
       if (this.$store.state.cart.platformTotals.discount_amount < 0) {
         grandTotalsValue = this.$store.state.cart.platformTotals.subtotal_incl_tax + this.$store.state.cart.platformTotals.shipping_incl_tax
